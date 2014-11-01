a4ba2f0281f3fecbe97295f851d611c097117ffa
Christopher Neufeld <github0000@cneufeld.ca>
2014-11-01 15:52:48 -0400

    Rewriting some memory/flag functions
    The knowledge of the special string "(i)" for memory and flags was
    showing up in places it really shouldn't have.  So, pushed all that
    into the memory and flag functions themselves.  The caller doesn't
    have to know that "(i)" is a special name that does special things.

--- stack.lisp
+++ stack.lisp
@@ -1,6 +1,11 @@
 ;; Stack operations for the HP-67 emulator
 ;;
 
+(defparameter *unlimited-indirection* nil
+  "If non-nil, the I-register is allowed to modify memory/flags outside the normally-permitted range")
+
+
+
 (define-condition not-real-number (error)
   ((val		:initarg value
                 :reader get-val))
@@ -9,6 +14,19 @@
              (format s "The complex value ~A was encountered."
                      (get-val c)))))
 
+(define-condition i-register-range-error (error)
+  ((val		:initarg value
+                :reader get-val)
+   (min		:initarg min-allowed
+                :reader get-min)
+   (max		:initarg max-allowed
+                :reader get-max))
+  (:documentation "I-register out of range for operation.")
+  (:report (lambda (c s)
+             (format s "The I-register was ~D.  In this context, it must lie between ~D and ~D"
+                     (get-val c) (get-min c) (get-max c)))))
+
+
 (defstruct (stack)
   (registers		(list 0 0 0 0))
   (registers-copy	nil)
@@ -16,7 +34,6 @@
   (last-x		nil)
 
   (memory		nil)
-  (register-i		0)
   (flags		'(("0" . nil)
                           ("1" . nil)
                           ("2" . nil)
@@ -28,115 +45,142 @@
 
 
 
-(defun memory-name (name)
-  (etypecase name
-    (string (copy-seq name))
-    (integer (format nil "~D" name))))
+(defun canonicalize-memory-name (stack mem-name)
+  (when (integerp mem-name)
+    (setf mem-name (format nil "~D" mem-name)))
+  (assert (stringp mem-name))
+  (cond
+    ((string-equal mem-name "(i)")
+     (multiple-value-bind (junk int-val str-val)
+         (get-i-register stack)
+       (declare (ignore junk))
+       (cond
+         ((and (not *unlimited-indirection*)
+               (or (< int-val 0) (> int-val 25)))
+          (error (make-condition 'i-register-range-error
+                                 :value int-val
+                                 :min-allowed 0
+                                 :max-allowed 25)))
+         ((= int-val 25)
+          "(i)")
+         ((> int-val 19)
+          (subseq "ABCDE"
+                  (- int-val 20)
+                  (- int-val 19)))
+         (t
+          str-val))))
+    (t
+     mem-name)))
 
-(defun convert-indirection-name (name)
-  (assert (numberp name))
-  (let ((num (floor name)))
-    (cond
-      ((or (< num 0) (> num 25))
-       "")
-      ((= num 25)
-       (values t t))
-      ((> num 19)
-       (subseq "ABCDE" (- num 20) (- num 19)))
-      (t
-       (format nil "~D" num)))))
 
-
-
-(defun store-memory (stack name val &key indirection)
-  (let (converted use-i-reg)
-    (cond
-      (indirection
-       (multiple-value-bind (c-name special-i-reg)
-           (convert-indirection-name name)
-         (setf use-i-reg special-i-reg
-               converted c-name)))
-      (t
-       (setf converted (memory-name name))))
-
-    (cond
-      (use-i-reg
-       (setf (stack-register-i stack) val))
-      (t
-       (setf (stack-memory stack)
-             (delete-duplicates
-              (push (cons converted val)
-                    (stack-memory stack))
-              :key 'car
-              :test 'string=
-              :from-end t)))))
+(defun store-memory-by-name (stack name val)
+  "Does no indirection, just stores under the name."
+  (setf (stack-memory stack)
+        (delete-duplicates
+         (push (cons name val)
+               (stack-memory stack))
+         :key 'car
+         :test 'string=
+         :from-end t))
   val)
 
-
-(defun recall-memory (stack name &key indirection)
-  (let (converted use-i-reg)
-    (cond
-      (indirection
-       (multiple-value-bind (c-name special-i-reg)
-           (convert-indirection-name name)
-         (setf use-i-reg special-i-reg
-               converted c-name)))
-      (t
-       (setf converted (memory-name name))))
-    (cond
-      (use-i-reg
-       (stack-register-i stack))
-      (t
-       (let ((record (assoc converted
-                            (stack-memory stack)
-                            :test 'string=)))
-         (if record
-             (cdr record)
-             0))))))
+(defun recall-memory-by-name (stack name)
+  "Does no indirection, just recalls from the name."
+  (let ((record (assoc name
+                       (stack-memory stack)
+                       :test 'string=)))
+    (if record
+        (cdr record)
+        0)))
 
 
-(defun set-flag-fcn (stack name &key clear)
-  (let* ((converted (memory-name name))
-         (record (assoc converted (stack-flags stack)
-                        :test 'string=)))
+(defun store-memory (stack name val)
+  (setf name (canonicalize-memory-name stack name))
+  (store-memory-by-name stack name val))
+
+
+(defun recall-memory (stack name)
+  (setf name (canonicalize-memory-name stack name))
+  (recall-memory-by-name stack name))
+
+
+
+(defun canonicalize-flag-name (stack flag-name)
+  (when (integerp flag-name)
+    (setf flag-name (format nil "~D" flag-name)))
+  (assert (stringp flag-name))
+  (cond
+    ((string-equal flag-name "(i)")
+     (multiple-value-bind (junk int-val str-val)
+         (get-i-register stack)
+       (declare (ignore junk))
+       (cond
+         ((and (not *unlimited-indirection*)
+               (or (< int-val 0) (> int-val 3)))
+          (error (make-condition 'i-register-range-error
+                                 :value int-val
+                                 :min-allowed 0
+                                 :max-allowed 3)))
+         (t
+          str-val))))
+    (t
+     flag-name)))
+  
+
+(defun set-flag-by-name (stack name &key clear)
+  (let ((record (assoc name (stack-flags stack)
+                       :test 'string=)))
     (cond
       (record
        (setf (cdr record) (not clear)))
       (t
        (setf (stack-flags stack)
-             (push (cons converted (not clear))
+             (push (cons name (not clear))
                    (stack-flags stack)))))))
+
+(defun get-flag-by-name (stack name)
+  (let* ((record (assoc name (stack-flags stack)
+                       :test 'string=))
+         (rval (cdr record)))
+    (when (or (string= name "2")
+              (string= name "3"))
+      (set-flag-by-name stack name :clear t))
+    rval))
+
+
+(defun set-flag-fcn (stack name &key clear)
+  (setf name (canonicalize-flag-name stack name))
+  (set-flag-by-name stack name :clear clear))
 
 (defun clear-flag-fcn (stack name)
   (set-flag-fcn stack name :clear t))
 
 (defun get-flag-fcn (stack name)
-  (let* ((converted (memory-name name))
-         (record (assoc converted (stack-flags stack)
-                        :test 'string=))
-         (rval (cdr record)))
-    (when (or (string= converted "2")
-              (string= converted "3"))
-      (clear-flag-fcn stack name))
-    rval))
+  (setf name (canonicalize-flag-name stack name))
+  (get-flag-by-name stack name))
+
 
 
 (defun set-i-register (stack value)
-  (setf (stack-register-i stack) value))
+  (store-memory-by-name stack "(i)" value))
 
+;; Returns 3 values.  The unmodified value of I, the greatest-integer
+;; value, and a string holding the greatest-integer value
 (defun get-i-register (stack)
-  (stack-register-i stack))
+  (let ((rval (recall-memory-by-name stack "(i)")))
+    (values
+     rval
+     (floor rval)
+     (format nil "~D" (floor rval)))))
+
+
 
 (defun swap-primary-secondary (stack)
   (dotimes (i 10)
-    (let ((val-prim (recall-memory stack i
-                                   :indirection t))
-          (val-second (recall-memory stack (+ i 10)
-                                     :indirection t)))
-      (store-memory stack i val-second
-                    :indirection t)
-      (store-memory stack (+ i 10) val-prim
-                    :indirection t))))
+    (let ((val-prim (recall-memory stack i))
+          (val-second (recall-memory stack (+ i 10))))
+      (store-memory stack i val-second)
+      (store-memory stack (+ i 10) val-prim))))
   
 
 
