f1e0cf989f59da9216c80d3f9b28977980b67ebf
Christopher Neufeld <github0000@cneufeld.ca>
2014-11-03 20:15:04 -0500

    Add a function to render rationals instead of using format
    Because using format to convert to scientific notation requires that
    numbers pass through a floating-point format, and because of the
    possible loss of precision there leading to incorrectly-displayed
    values, a new function has been added to perform the equivalent
    operation by long-hand division.  Emulates the "~,vE" format control
    string.

--- display.lisp
+++ display.lisp
@@ -8,21 +8,132 @@
 ;; There is also some automatic mode switching if the number can't be
 ;; displayed in the 'fixed' display mode.
 
-(defun format-for-printing (state val &key readable)
+(defun format-for-printing (state val)
   (let ((digits-after-decimal (get-display-digits state)))
     (ecase (get-display-output-mode state)
       (:FIXED
-       (format-for-printing-fix val
-                                digits-after-decimal
-                                :readable readable))
+       (format-for-printing-fix (rational val)
+                                digits-after-decimal))
       (:SCIENTIFIC
-       (format-for-printing-sci val
-                                digits-after-decimal
-                                :readable readable))
+       (format-for-printing-sci (rational val)
+                                digits-after-decimal))
       (:ENGINEERING
-       (format-for-printing-eng val
-                                digits-after-decimal
-                                :readable readable)))))
+       (format-for-printing-eng (rational val)
+                                digits-after-decimal)))))
+
+
+;; n-digits is the number after the decimal
+(defun render-rational-as-sci (rval n-digits)
+  "In the end, we can't trust format because of its rounding rules, and the coercion.  Even going to double-float can't guarantee that we won't slip a digit in the last place.  So, here we 'display' a rational number by longhand division."
+  (when (= rval 0)
+    (return-from render-rational-as-sci
+      (format nil "~,vE" n-digits 0.0)))
+
+  (let* ((result '())
+         (rv (make-string-output-stream))
+         (sign (if (> rval 0) 1 -1))
+         (num (numerator (abs rval)))
+         (den (denominator (abs rval)))
+         (exponent 0))
+    ;; First, adjust the numerator or denominator so that the rational
+    ;; is at least 1 and less than 10.
+    
+    (do ()
+        ((>= num den))
+      (setf num (* 10 num))
+      (decf exponent))
+    (do ()
+        ((< num (* 10 den)))
+      (setf den (* 10 den))
+      (incf exponent))
+
+    (dotimes (i (1+ n-digits))
+      (let ((digit (floor (/ num den))))
+        (push digit result)
+        (decf num (* digit den))
+        (setf num (* 10 num))))
+
+    (let ((carry (if (>= (/ num den) (/ 1 2)) 1 0)))
+      (setf result (mapcar #'(lambda (x)
+                               (setf x (+ carry x))
+                               (cond
+                                 ((= 10 x)
+                                  (setf x 0))
+                                 (t
+                                  (setf carry 0)))
+                               x) result))
+
+      (cond
+        ((= carry 1)
+         ;; rounded all the way to the beginning. Fix.
+         (pop result)
+         (setf result (reverse result))
+         (incf exponent)
+         (push 1 result))
+        (t
+         (setf result (reverse result)))))
+
+    (format rv "~A~D."
+            (if (= sign -1) "-" "")
+            (car result))
+    (format rv "~{~D~}" (cdr result))
+    (format rv "e~A~2,'0D"
+            (if (< exponent 0) "-" "")
+            (abs exponent))
+
+    (get-output-stream-string rv)))
+
+
+
+
+(defun make-rational-from-float-string (string)
+  "This all has to happen without passing through floats"
+  (let ((d-pos (position #\. string))
+        whole-part frac-part)
+    (cond
+      (d-pos
+       (setf whole-part (subseq string 0 d-pos))
+       (when (= (length whole-part) 0)
+         (setf whole-part "0"))
+       (setf frac-part (subseq string (1+ d-pos))))
+      (t
+       (setf whole-part (copy-seq string))))
+
+    ;; switch it from string to integer
+    (setf whole-part (read-from-string whole-part))
+    (cond
+      (frac-part
+       (let ((frac-len (length frac-part))
+             (divisor 1))
+         (dotimes (i frac-len)
+           (setf divisor (* 10 divisor)))
+         (setf frac-part (/ (read-from-string frac-part)
+                            divisor)))
+       (when (< whole-part 0)
+         (setf frac-part (* -1 frac-part))))
+      (t
+       (setf frac-part 0)))
+
+    (+ whole-part frac-part)))
+
+
+(defun make-rational-from-sci-string (string)
+  (destructuring-bind (sign mantissa e-sign expt)
+      (break-down-sci-notation string)
+    (let ((mant-rat (make-rational-from-float-string mantissa))
+          (neg-mant (string= sign "-"))
+          (neg-expt (string= e-sign "-"))
+          (expt-int (read-from-string expt))
+          (expt-factor 1))
+
+      (if neg-expt
+          (dotimes (i expt-int)
+            (setf expt-factor (/ expt-factor 10)))
+          (dotimes (i expt-int)
+            (setf expt-factor (* expt-factor 10))))
+
+      (* mant-rat expt-factor (if neg-mant -1 1)))))
+
 
 
 (defun string-contains-non-zero-digit (string)
@@ -103,23 +214,24 @@
             expt)))
 
 
-(defun format-for-printing-fix (val digits-after-decimal
-                                &key readable)
-
+(defun format-for-printing-fix (val digits-after-decimal)
+  ;; 'val' arrives as rational, and we do our best to represent it
+  (assert (rationalp val))
   (when (= val 0)
     (return-from format-for-printing-fix
-      (format nil "~,vF" digits-after-decimal 0.0d0)))
-  
-  (let* ((negmult (if (< val 0) -1.0d0 1.0d0))
-         (scaleup (expt 10.0d0 digits-after-decimal))
+      (values (format nil "~,vF" digits-after-decimal 0.0d0)
+              0)))
+
+  (let* ((negmult (if (< val 0) -1 1))
+         (scaleup (expt 10 digits-after-decimal))
          (magnitude (abs val))
          (rounded (* negmult
-                     (floor (+ 0.50000000004d0
+                     (floor (+ (/ 1 2)
                                (* magnitude scaleup)))))
          (first-try (format nil "~,v,vF"
                             digits-after-decimal
                             (- digits-after-decimal)
-                            rounded))
+                            (coerce rounded 'double-float)))
          (max-width (+ 1 *digits-in-display*
                        (if (< val 0) 1 0))))
 
@@ -129,66 +241,83 @@
               (<= overrun digits-after-decimal))
          (format-for-printing-fix val
                                   (- digits-after-decimal
-                                     overrun)
-                                  :readable readable))
+                                     overrun)))
         ((> overrun 0)
-         (format-for-printing-sci val digits-after-decimal
-                                  :readable readable))
+         (format-for-printing-sci val digits-after-decimal))
         ((and (/= val 0)
               (not (string-contains-non-zero-digit first-try)))
-         (format-for-printing-sci val digits-after-decimal
-                                  :readable readable))
+         (format-for-printing-sci val digits-after-decimal))
         (t
-         first-try)))))
+         (values first-try
+                 (make-rational-from-float-string first-try)))))))
 
 
 
-(defun format-for-printing-sci (val digits-after-decimal
-                                &key readable)
+(defun format-for-printing-sci (val digits-after-decimal)
+  (assert (rationalp val))
   (when (= 0 val)
     (return-from format-for-printing-sci
-      (if readable
-          "0.0d0"
-          (format nil "~,vE" digits-after-decimal 0.0d0))))
+      (values (format nil "~,vE" digits-after-decimal 0.0) 0)))
   
+  (let* ((magnitude (abs val))
+         (maglog (floor (log magnitude 10.0d0)))
+         (scaleup (- digits-after-decimal maglog))
+         (rounded (* (floor (+ (/ 1 2)
+                               (* magnitude (expt 10 scaleup))))
+                     (expt 10 (- scaleup))))
+
+         (first-try (format nil "~A~,v,2E"
+                            (if (< val 0) "-" "")
+                            digits-after-decimal
+                            (coerce rounded 'double-float)))
+         formatted f-rat)
+
+    (setf first-try (round-sci-notation-to-digits first-try
+                                                  digits-after-decimal))
+
+    (destructuring-bind (sign mantissa e-sign exponent)
+        (break-down-sci-notation first-try)
+
+      (setf formatted
+            (format nil "~A~vA~A~A"
+                    sign
+                    (1+ *digits-in-display*)
+                    mantissa
+                    e-sign
+                    exponent))
+
+      (setf f-rat
+            (format nil "~A~Ad~A~A"
+                    (if (string= sign "-")
+                        "-"
+                        "")
+                    mantissa
+                    (if (string= e-sign "-")
+                        "-"
+                        "")
+                    exponent)))
+    
+
+    (values formatted (make-rational-from-sci-string f-rat))))
+    
+    
+(defun format-for-printing-eng (val digits-after-decimal)
+  (when (= 0 val)
+    (return-from format-for-printing-eng
+      (values (format nil "~,vE" digits-after-decimal 0.0) 0)))
+
   (let* ((magnitude (abs val))
          (first-try (format nil "~A~,v,2E"
                             (if (< val 0) "-" "")
                             digits-after-decimal
                             magnitude))
-         formatted)
+         formatted f-rat)
 
     (setf first-try (round-sci-notation-to-digits first-try
                                                   digits-after-decimal))
 
-    (unless readable
-      (destructuring-bind (sign mantissa e-sign exponent)
-          (break-down-sci-notation first-try)
-
-        (setf formatted
-              (format nil "~A~vA~A~A"
-                      sign
-                      (1+ *digits-in-display*)
-                      mantissa
-                      e-sign
-                      exponent))))
-
-    (if readable
-        (values first-try first-try)
-        (values formatted first-try))))
-    
-    
-(defun format-for-printing-eng (val digits-after-decimal
-                                &key readable)
-  (multiple-value-bind (junk parsed)
-      (format-for-printing-sci val digits-after-decimal
-                               :readable readable)
-    (declare (ignore junk))
-    (when readable
-      (return-from format-for-printing-eng parsed))
-
     (destructuring-bind (sign mantissa e-sign exponent)
-        (break-down-sci-notation parsed)
+        (break-down-sci-notation first-try)
 
       (let* ((e-num (read-from-string exponent))
              (man-len (length mantissa))
@@ -206,11 +335,23 @@
           (setf e-num (* -1 e-num)))
         (decf e-num shift-num)
 
-        (format nil "~A~vA~A~2,'0D"
-                sign
-                (1+ *digits-in-display*)
-                mantissa
-                e-sign
-                (abs e-num))))))
+        (setf formatted (format nil "~A~vA~A~2,'0D"
+                                sign
+                                (1+ *digits-in-display*)
+                                mantissa
+                                e-sign
+                                (abs e-num)))
+        (setf f-rat
+              (format nil "~A~Ad~A~A"
+                      (if (string= sign "-")
+                          "-"
+                          "")
+                      mantissa
+                      (if (string= e-sign "-")
+                          "-"
+                          "")
+                      e-num))))
+
+    (values formatted (make-rational-from-sci-string f-rat))))
 
         
