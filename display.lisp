4db212d6bcca21a9b9fd3d7c0d92cd113330b17c
Christopher Neufeld <github0000@cneufeld.ca>
2014-11-05 20:45:26 -0500

    More rational type support work
    Moved the long division calculation into its own function, fixed a
    carry bug in it.  Added a third element to the returned list, a number
    which is 1 if the least-significant digit in the returned list of
    digits was rounded up to reach that value, or rounded down.  This is
    of interest when the least significant digit is 5, and further
    rounding is required.
    
    Wrote a function to perform iterative rounding of the digital
    representation of a ratio, correctly.
    
    Added functions to perform the equivalent of ~E and ~F format
    operations on rational numbers, with no loss of precision or accuracy,
    in a format compatible with the behaviour of the HP-67 calculator.

--- display.lisp
+++ display.lisp
@@ -22,21 +22,26 @@
                                 digits-after-decimal)))))
 
 
-;; n-digits is the number after the decimal
-(defun render-rational-as-sci (rval n-digits)
-  "In the end, we can't trust format because of its rounding rules, and the coercion.  Even going to double-float can't guarantee that we won't slip a digit in the last place.  So, here we 'display' a rational number by longhand division."
-  (when (= rval 0)
-    (return-from render-rational-as-sci
-      (format nil "~,vE" n-digits 0.0)))
+(defun position-of-exponent-marker (string)
+  (position-if #'(lambda (x)
+                   (or (char-equal x #\e)
+                       (char-equal x #\d))) string))
 
-  (let* ((result '())
-         (rv (make-string-output-stream))
-         (sign (if (> rval 0) 1 -1))
-         (num (numerator (abs rval)))
-         (den (denominator (abs rval)))
-         (exponent 0))
-    ;; First, adjust the numerator or denominator so that the rational
-    ;; is at least 1 and less than 10.
+
+(defun perform-long-division (ratval n-digits)
+  "Computes the decimal expansion of the positive rational to n total digits, with appropriate rounding.  Returns a list of three values.  The first is a list of digits, from most to least significant.  The second is the power of 10 on the first digit.  The third is 1 if the least-significant digit was rounded up to that value (0 means it rounded down)."
+  (assert (>= ratval 0))
+
+  (assert (>= n-digits 0))
+  (when (= ratval 0)
+    (return-from perform-long-division
+      (list (make-list n-digits :initial-element 0) 0)))
+
+  (let ((result '())
+        (num (numerator ratval))
+        (den (denominator ratval))
+        (exponent 0)
+        carryflag)
     
     (do ()
         ((>= num den))
@@ -47,13 +52,20 @@
       (setf den (* 10 den))
       (incf exponent))
 
-    (dotimes (i (1+ n-digits))
-      (let ((digit (floor (/ num den))))
-        (push digit result)
-        (decf num (* digit den))
-        (setf num (* 10 num))))
+    (cond
+      ((> n-digits 0)
+       (dotimes (i n-digits)
+         (let ((digit (floor (/ num den))))
+           (push digit result)
+           (decf num (* digit den))
+           (setf num (* 10 num)))))
+      ((= n-digits 0)
+       (incf exponent)
+       (setf result (list 0))))
 
+    (setf num (/ num 10))  ;; erase the extra power of 10 we put in
     (let ((carry (if (>= (/ num den) (/ 1 2)) 1 0)))
+      (setf carryflag carry)
       (setf result (mapcar #'(lambda (x)
                                (setf x (+ carry x))
                                (cond
@@ -73,16 +85,172 @@
         (t
          (setf result (reverse result)))))
 
+    (list result exponent carryflag)))
+
+
+(defun round-long-division-result (ld-res)
+  "Rounds to remove the least-significant digit."
+  (labels
+      ((round-up (remaining-reversed-digits expt)
+         (let ((carry 1)
+               unreversed)
+           (setf remaining-reversed-digits
+                 (mapcar #'(lambda (x)
+                             (incf x carry)
+                             (setf carry 0)
+                             (when (= x 10)
+                               (setf x 0)
+                               (setf carry 1))
+                             x) remaining-reversed-digits))
+
+           (setf unreversed (reverse remaining-reversed-digits))
+           (when (= carry 1)
+             (push 1 unreversed)
+             (incf expt))
+
+           (list unreversed expt 1))))
+
+    (let* ((digits (reverse (first ld-res)))
+           (exponent (second ld-res))
+           (carryflag (third ld-res))
+           (least-sig (pop digits)))
+
+      (cond
+        (digits
+         (cond
+           ((< least-sig 5)
+            (list (reverse digits) exponent 0))
+           ((> least-sig 5)
+            (round-up digits exponent))
+           ((= carryflag 1)
+            (list (reverse digits) exponent 0))
+           ((= carryflag 0)
+            (round-up digits exponent))
+           (t
+            (error "bad flow, shouldn't get here"))))
+
+        ;; When we reach the final digit, we round to 0 or 1, not to
+        ;; nil
+        (t
+         (cond
+           ((or (< least-sig 5)
+                (and (= least-sig 5)
+                     (= carryflag 1)))
+            (list (list 0) 0 0))
+           (t
+            (list (list 1) 0 1))))))))
+
+
+
+;; n-digits is the number after the decimal
+(defun render-rational-as-sci (rval n-digits
+                               &key (truncating t))
+  "In the end, we can't trust format because of its rounding rules, and the coercion.  Even going to double-float can't guarantee that we won't slip a digit in the last place.  So, here we 'display' a rational number by longhand division."
+  (when (and truncating
+             (> (1+ n-digits) *digits-in-display*))
+    (setf n-digits (1- *digits-in-display*)))
+
+  (when (= rval 0)
+    (return-from render-rational-as-sci
+      (format nil "~,vE" n-digits 0.0)))
+
+  (let* ((rv (make-string-output-stream))
+         (sign (if (> rval 0) 1 -1))
+         (ld (perform-long-division (abs rval) (1+ n-digits)))
+         (digits (first ld))
+         (exponent (second ld)))
+
     (format rv "~A~D."
             (if (= sign -1) "-" "")
-            (car result))
-    (format rv "~{~D~}" (cdr result))
+            (car digits))
+    (format rv "~{~D~}" (cdr digits))
     (format rv "e~A~2,'0D"
             (if (< exponent 0) "-" "")
             (abs exponent))
 
     (get-output-stream-string rv)))
 
+
+;; The 'truncating' keyword, if non-nil, bounds the total number of
+;; digits to *digits-in-display*
+(defun render-rational-as-fix (rval n-digits-after-decimal
+                               &key (truncating t))
+
+  (assert (>= n-digits-after-decimal 0))
+
+  (when (= rval 0)
+    (return-from render-rational-as-fix
+      (format nil "~,vF" n-digits-after-decimal 0.0)))
+
+  (let ((sign (if (> rval 0) 1 -1))
+        (rabs (abs rval))
+        magnitude
+        digits-needed)
+        
+
+    ;; Check to see if, following rounding, the number won't fit in
+    ;; the width of the display.  If so, use the scientific notation
+    ;; function instead
+    (when (and truncating
+               (or (>= rabs (- (expt 10 *digits-in-display*) (/ 1 2)))
+                   (< rabs (/ (expt 10 (- n-digits-after-decimal)) 2))))
+      (return-from render-rational-as-fix
+        (render-rational-as-sci rval n-digits-after-decimal
+                                :truncating truncating)))
+
+
+    ;; We'll use a log to estimate the magnitude, but may correct it
+    ;; if rounding errors give the wrong result
+    (setf magnitude (floor (log (coerce rabs 'double-float) 10)))
+    (when (>= rabs (expt 10 (1+ magnitude)))
+      (incf magnitude))
+    (when (< rabs (expt 10 magnitude))
+      (decf magnitude))
+
+    (setf digits-needed (+ magnitude n-digits-after-decimal 1))
+
+    (let* ((ld (perform-long-division rabs digits-needed))
+           (digits (first ld))
+           (exponent (second ld))
+           (rv (make-string-output-stream)))
+
+      ;; Cut off digits that are past the end of the display, by
+      ;; rounding exactly
+      (when (and truncating
+                 (< rabs 1))
+        (let ((width (+ -1 (length digits) (- exponent))))
+          (dotimes (i (- width (min n-digits-after-decimal
+                                    (1- *digits-in-display*))))
+            (setf ld (round-long-division-result ld))))
+        (setf digits (first ld))
+        (setf exponent (second ld)))
+
+      (when (and truncating
+                 (> rabs 1))
+        (let ((width (length digits)))
+          (dotimes (i (- width *digits-in-display*))
+            (setf ld (round-long-division-result ld))))
+        (setf digits (first ld))
+        (setf exponent (second ld)))
+
+      ;; put in leading zeroes, they aren't in the division result
+      (when (< sign 0)
+        (format rv "-"))
+      (when (< exponent 0)
+        (format rv "0.")
+        (dotimes (i (- -1 exponent))
+          (format rv "0")))
+
+      (dotimes (i (length digits))
+        (format rv "~D" (nth i digits))
+        (when (= i exponent)
+          (format rv ".")))
+
+      (let ((result (get-output-stream-string rv)))
+        (if (not (string-contains-non-zero-digit result))
+            (render-rational-as-sci rval n-digits-after-decimal
+                                    :truncating truncating)
+            result)))))
 
 
 
@@ -146,9 +314,7 @@
 
 (defun break-down-sci-notation (string)
   (let* ((negative (char= (char string 0) #\-))
-         (epos (position-if #'(lambda (x)
-                                (or (char= x #\e)
-                                    (char= x #\d))) string))
+         (epos (position-of-exponent-marker string))
          (neg-expt (char= (char string (1+ epos)) #\-))
          (mantissa (subseq string
                            (if negative 1 0)
@@ -167,22 +333,42 @@
 
 (defun shift-char-to-right (string start-pos n-shift
                             &key (padding #\0))
-  "Moves the character at start-pos n-shift to the right"
+  "Moves the character at start-pos n-shift to the right.  Accepts negative arguments for movement to the left"
+  (when (= n-shift 0)
+    (return-from shift-char-to-right (copy-seq string)))
+  
   (let ((workspace (copy-seq string))
         (moved (char string start-pos))
         (pad-len (- (+ 1 start-pos n-shift) (length string))))
 
-    (when (> pad-len 0)
-      (setf workspace
-            (concatenate 'string
-                         workspace
-                         (make-sequence 'string
-                                        pad-len
-                                        :initial-element padding))))
-    (dotimes (i n-shift)
-      (setf (char workspace (+ i start-pos))
-            (char workspace (+ i 1 start-pos)))
-      (setf (char workspace (+ i 1 start-pos)) moved))
+    (cond
+      ((and (> n-shift 0)
+            (> pad-len 0))
+       (setf workspace
+             (concatenate 'string
+                          workspace
+                          (make-sequence 'string
+                                         pad-len
+                                         :initial-element padding))))
+      ((and (< n-shift 0)
+            (> (- n-shift) (1- start-pos)))
+       (let ((pad-len (- 0 n-shift start-pos -1)))
+       (setf workspace
+             (concatenate 'string
+                          (make-sequence 'string
+                                         pad-len
+                                         :initial-element padding)
+                          workspace))
+       (incf start-pos pad-len))))
+
+    (let ((inc-op (if (> n-shift 0) #'+ #'-)))
+      (when (< n-shift 0)
+        (setf n-shift (- n-shift)))
+      (dotimes (i n-shift)
+        (setf (char workspace (funcall inc-op start-pos i))
+              (char workspace (funcall inc-op start-pos (1+ i))))
+        (setf (char workspace (funcall inc-op start-pos (1+ i))) moved)))
+
     workspace))
 
 
