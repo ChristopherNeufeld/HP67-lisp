3c227efa97898db02777ca6ba9acbd03537bdcc1
Christopher Neufeld <github0000@cneufeld.ca>
2014-11-17 20:44:08 -0500

    Moved a few constants into their own file
    Put some constants here, as they are used in more than one place.

---
+++ constants.lisp
@@ -0,0 +1,8 @@
+;; Constants used in the program
+
+(defparameter *digits-in-display* 10)
+(defparameter *overflow-exponent* 99)  ;; larger than this, and
+                                       ;; there's an error
+
+
+
