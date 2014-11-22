79a8f5f29122a6b573a51ef3874c8b1b5402e6fa
Christopher Neufeld <github0000@cneufeld.ca>
2014-11-22 10:21:51 -0500

    Set up key layout for NCurses CLI
    The basic system is now in place for displaying the selectable keys in
    the CLI.

--- curses-cli.lisp
+++ curses-cli.lisp
@@ -1,18 +1,20 @@
 ;; The CL-CHARMS (NCurses) interface to the HP-67 emulator
 ;;
 
+(declaim (optimize (debug 3) (safety 3)))
+
 (defpackage :CURSES-CLI
   (:use :COMMON-LISP :ASDF/INTERFACE)
   (:import-from :HP67-INTERNALS
                 :GET-NEW-STACK-OBJECT
                 :GET-NEW-MODE-OBJECT
                 :GET-KEY-ABBREVS
-                :GET-ACTIVE-KEY-ABBREVS
                 :KEY-STRUCT-ABBREV
                 :KEY-STRUCT-KEY-LOCATION
                 :LOCATION-CATEGORY-1
                 :LOCATION-CATEGORY-2
-                :LOCATION-COL :LOCATION-ROW)
+                :LOCATION-COL :LOCATION-ROW
+                :LOCATION-NARROW-KEY)
   (:export :main))
 
 
@@ -21,7 +23,11 @@
 (defparameter *excluded-keys*
   '("0" "1" "2" "3" "4"
     "5" "6" "7" "8" "9"
-    "eex" "enter" "(i)"))
+    "eex" "enter" "(i)" "."
+    "GSB-A" "GSB-B" "GSB-C" "GSB-D" "GSB-E"
+    "GSB-a" "GSB-b" "GSB-c" "GSB-d" "GSB-e"
+    "label-L"))
+
 
 (defparameter *category-precedence*
   '(:ARITHMETIC :ALGEBRAIC
@@ -33,80 +39,95 @@
                                       
 
 
-(defun remove-excluded-keys (list)
-  (remove-if #'(lambda (x)
-                 (member (key-struct-abbrev x)
-                         *excluded-keys*))
-             list))
+(defun comp< (key1 key2)
+  (let* ((loc1 (key-struct-key-location key1))
+         (loc2 (key-struct-key-location key2))
 
+         (narrow-1 (location-narrow-key loc1))
+         (narrow-2 (location-narrow-key loc2))
 
-(defun sort-keys (keylist)
-  (labels
-      ((comp< (key1 key2)
-         (let* ((loc1 (key-struct-key-location key1))
-                (loc2 (key-struct-key-location key2))
+         (cat1-1 (location-category-1 loc1))
+         (cat2-1 (location-category-2 loc1))
+         (cat1-2 (location-category-1 loc2))
+         (cat2-2 (location-category-2 loc2))
 
-                (cat1-1 (location-category-1 loc1))
-                (cat2-1 (location-category-2 loc1))
-                (cat1-2 (location-category-1 loc2))
-                (cat2-2 (location-category-2 loc2))
+         (row1 (location-row loc1))
+         (row2 (location-row loc2))
+         (col1 (location-col loc1))
+         (col2 (location-col loc2)))
 
-                (row1 (location-row loc1))
-                (row2 (location-row loc2))
-                (col1 (location-col loc1))
-                (col2 (location-col loc2)))
-
-           (cond
-             ((not (eq cat1-1 cat1-2))
-              (> (length (member cat1-1 *category-precedence*))
-                 (length (member cat1-2 *category-precedence*))))
-             ((and (not cat2-1) cat2-2)
-              t)
-             ((and cat2-1 (not cat2-2))
-              nil)
-             ((and cat2-1 cat2-2)
-              (string< (symbol-name cat2-1)
-                       (symbol-name cat2-2)))
-             ((and row1 row2)
-              (or (< row1 row2)
-                  (and (= row1 row2)
-                       (< col1 col2))))
-             (t
-              (string< (key-struct-abbrev key1)
-                       (key-struct-abbrev key2)))))))
-
-    (sort keylist #'comp<)))
-
-              
+    (cond
+      ((not (eq cat1-1 cat1-2))  ;; different categories
+       (> (length (member cat1-1 *category-precedence*))
+          (length (member cat1-2 *category-precedence*))))
+      ((and (not cat2-1) cat2-2) ;; 1 has a sub-category
+       t)
+      ((and cat2-1 (not cat2-2)) ;; 2 has a sub-category
+       nil)
+      ((and cat2-1 cat2-2)  ;; both have sub-categories
+       (string< (symbol-name cat2-1)
+                (symbol-name cat2-2)))
+      ((and narrow-1 (not narrow-2))
+       t)
+      ((and narrow-2 (not narrow-1))
+       nil)
+      ((and row1 row2)
+       (or (< row1 row2)
+           (and (= row1 row2)
+                (< col1 col2))))
+      (t
+       (string< (key-struct-abbrev key1)
+                (key-struct-abbrev key2))))))
 
 
 
         
 (defun main()
   (charms:with-curses ()
-    (charms:enable-echoing)
+    (charms:disable-echoing)
     (charms:disable-extra-keys charms:*standard-window*)
-    (charms:disable-non-blocking-mode charms:*standard-window*)
+    (charms:enable-non-blocking-mode charms:*standard-window*)
     (charms:enable-raw-input :interpret-control-characters t)
 
-    (let* ((stack (get-new-stack-object 4))
-           (mode (get-new-mode-object))
-           (all-keys (sort-keys
-                      (remove-excluded-keys
-                       (get-key-abbrevs))))
-           (maxlen (apply 'max
-                          (mapcar
-                           #'(lambda (x)
-                               (length (key-struct-abbrev x)))
-                           all-keys))))
+    (multiple-value-bind (n-cols n-rows)
+        (charms:window-dimensions charms:*standard-window*)
 
-      (do (exit-requested)
-          (exit-requested)
+      (let* ((stack (get-new-stack-object 4))
+             (mode (get-new-mode-object))
+             (all-keys (get-key-abbrevs mode
+                                        :sort-fcn #'comp<
+                                        :veto-list *excluded-keys*))
+             (maxlen (apply 'max
+                            (mapcar 'length all-keys)))
+             (keys-per-row (floor (/ n-cols (1+ maxlen)))))
 
-        (let ((active-keys (sort-keys
-                            (remove-excluded-keys
-                             (get-active-key-abbrevs mode)))))
-          
-          
-    
-        )))))
+        (do (exit-requested)
+            (exit-requested)
+
+          (charms:clear-window charms:*standard-window*)
+
+          (let ((active-keys (get-key-abbrevs mode
+                                              :sort-fcn #'comp<
+                                              :veto-list *excluded-keys*
+                                              :limit-to-mode t))
+                (i 0))
+
+            (dolist (candidate all-keys)
+              (when (member candidate active-keys
+                            :test 'string=)
+                (multiple-value-bind (r c)
+                    (floor i keys-per-row)
+                  (charms:move-cursor charms:*standard-window*
+                                      (* c (1+ maxlen))
+                                      r))
+                (charms:write-string-at-cursor charms:*standard-window*
+                                               candidate))
+              (incf i))
+
+            (charms:refresh-window charms:*standard-window*)
+
+            (sleep 4)
+
+            (setf exit-requested t)
+
+            ))))))
