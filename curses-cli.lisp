d06726d9aaf18ae1e4821f6717596d85858e8e49
Christopher Neufeld <github0000@cneufeld.ca>
2014-12-03 15:30:57 -0500

    Rewriting the ncurses CLI for the UI class model
    Broke up the old event loop into separate generic functions according
    to the UI class.  Next, we have to teach the engine how to use the new
    interface.

--- curses-cli.lisp
+++ curses-cli.lisp
@@ -20,10 +20,32 @@
                 :LOCATION-CATEGORY-2
                 :LOCATION-COL :LOCATION-ROW
                 :LOCATION-NARROW-KEY)
+  (:import-from :HP67-UI
+                :GET-ACTIVE-KEYS
+                :GET-ACTIVE-SHIFT
+                :GET-ACTIVE-MODE
+                :GET-DISPLAY-MODE
+                :GET-DISPLAY-DIGITS
+                :GET-ERROR-TEXT
+                :GET-STACK-REAL-CONTENTS
+                :GET-STACK-IMAG-CONTENTS
+                :GET-MEMORY-CONTENTS
+                :GET-PROGRAM-CONTENTS
+                :GET-PROGRAM-COUNTER
+                :GET-QUIT-REQUESTED
+                :GET-COMPLEX-MODE
+                :UI-HAS-ERROR-TEXT
+                :UI-SET-ACTIVE-MODE
+                :UI-PAINT
+                :UI-GET-INPUT
+                )
+
   (:export :main))
 
 
 (in-package :CURSES-CLI)
+
+
 
 (defparameter *excluded-keys*
   '("0" "1" "2" "3" "4"
@@ -45,6 +67,208 @@
     :MEMORY :INDIRECTION :MODE-SWITCH
     :FLAGS :FLOW_CONTROL :PROGRAM-MEMORY
     :EXTERNAL-I-O))
+
+
+(defparameter *max-mem-label-length* 10)
+
+(defclass curses-cli (hp67-ui:ui-base)
+  ((window		:accessor get-window
+                        :initarg :window
+                        :initform nil)
+   (rows		:accessor get-rows
+                        :initarg :rows
+                        :initform 0)
+   (cols		:accessor get-cols
+                        :initarg :cols
+                        :initform 0)
+
+   (all-keys		:accessor get-all-keys)
+   (active-keys		:accessor get-active-keys)
+
+   (button-rows		:accessor get-button-rows
+                        :initform 0)
+   (button-width	:accessor get-button-width
+                        :initform 0)
+   (keys-per-row	:accessor get-keys-per-row
+                        :initform 0)
+   (error-text-row	:accessor get-error-row
+                        :initform 0)
+
+   (stack-disp-size	:accessor get-stack-display-size
+                        :initform 0)
+   (stack-row-1		:accessor get-stack-first-row
+                        :initform 0)
+   (stack-col-1		:accessor get-stack-column
+                        :initform 0)
+   
+   (mem-disp-size	:accessor get-mem-display-size
+                        :initform 0)
+   (mem-row-1		:accessor get-mem-first-row
+                        :initform 0)
+   (mem-label-col	:accessor get-mem-label-column
+                        :initform 0)
+   (mem-value-col	:accessor get-mem-value-column
+                        :initform (+ 2 *max-mem-label-length*))
+
+   (input-row		:accessor get-input-row
+                        :initform 0)
+   (input-col		:accessor get-input-col
+                        :initform 0)))
+
+
+(defun fill-in-parameters (object all-keys key-width)
+  "Given a curses-cli object with window, rows, and columns set,
+fills in the rest of the parameters appropriately."
+  (let* ((num-keys (length all-keys))
+         (key-rows (1+ (floor num-keys (1+ key-width)))))
+    
+    (setf (get-button-rows object) key-rows)
+    (setf (get-button-width object) (1+ key-width))
+    (setf (get-keys-per-row object) (floor (get-cols object) key-width))
+    (setf (get-all-keys object) all-keys)
+
+    (setf (get-error-row object) (+ 2 key-rows))
+
+    (setf (get-input-row object) (- (get-rows object) 2)
+          (get-input-col object) 0)
+
+    (setf (get-stack-display-size object)
+          (- (get-rows object) key-rows 6))
+    (setf (get-stack-first-row object)
+          (+ key-rows 4))
+    (setf (get-stack-column object) 0)
+
+    (setf (get-mem-display-size object)
+          (- (get-rows object) key-rows 6))
+    (setf (get-mem-first-row object)
+          (+ key-rows 4))
+    (setf (get-mem-label-column object) (floor (get-cols object) 2))
+    (setf (get-mem-value-column object) (+ 2 *max-mem-label-length*
+                                           (get-mem-label-column object)))))
+    
+
+(defun main-2 ()
+  (charms:with-curses ()
+    (let* ((w charms:*standard-window*)
+           (dims (multiple-value-list (charms:window-dimensions w)))
+           (curses-obj
+            (make-instance 'curses-cli
+                           :window w
+                           :rows (second dims)
+                           :cols (first dims)))
+
+           (all-keys (get-key-abbrevs nil
+                                      :sort-fcn #'comp<
+                                      :veto-list *excluded-keys*))
+           (maxlen (apply 'max
+                          (mapcar 'length all-keys))))
+
+      (fill-in-parameters curses-obj all-keys maxlen)))
+
+  (error "continue"))
+
+
+(defmethod hp67-ui:ui-set-active-mode :after ((ui curses-cli) active-mode)
+  (setf (get-active-keys ui)
+        (get-key-abbrevs active-mode
+                         :sort-fcn #'comp<
+                         :veto-list *excluded-keys*
+                         :limit-to-mode t)))
+
+(defmethod hp67-ui:ui-paint ((ui curses-cli))
+  (charms:clear-window (get-window ui))
+  (let ((i 0))
+    (dolist (candidate (get-all-keys ui))
+      (when (member candidate (get-active-keys ui)
+                    :test 'string=)
+        (multiple-value-bind (r c)
+            (floor i (get-keys-per-row ui))
+          (charms:move-cursor (get-window ui)
+                              (* c (get-button-width ui))
+                              r))
+        (charms:write-string-at-cursor (get-window ui) candidate))
+      (incf i)))
+
+  (when (ui-has-error-text ui)
+    (charms:move-cursor (get-window ui) 0 (get-error-row ui))
+    (charms:write-string-at-cursor (get-window ui)
+                                   (get-error-text ui)))
+
+  (when (get-complex-mode ui)
+    (error "Have to write complex stack output code."))
+
+  (let* ((stack-contents (get-stack-real-contents ui))
+         (n-entries (array-dimension stack-contents 0)))
+    (dotimes (i n-entries)
+      (let ((display-row (- (+ (get-stack-first-row ui)
+                               (get-stack-display-size ui))
+                            (1+ i))))
+      (when (< i (get-stack-display-size ui))
+        (charms:move-cursor (get-window ui)
+                            (get-stack-column ui)
+                            display-row)
+        (charms:write-string-at-cursor (get-window ui)
+                                       (aref stack-contents i))))))
+  
+  (when (get-complex-mode ui)
+    (error "Have to write complex memory output code."))
+
+  (let* ((mem-contents (get-memory-contents ui))
+         (n-entries (length mem-contents)))
+    (dotimes (i n-entries)
+      (let ((display-row (- (+ (get-mem-first-row ui)
+                               (get-mem-display-size ui))
+                            (1+ i))))
+      (when (< i (get-mem-display-size ui))
+        (charms:move-cursor (get-window ui)
+                            (get-mem-label-column ui)
+                            display-row)
+        (let ((label (copy-seq (second (aref mem-contents i)))))
+          (when (> (length label) *max-mem-label-length*)
+            (setf label (subseq label 0 *max-mem-label-length*)))
+          (charms:write-string-at-cursor (get-window ui) label))
+
+        (charms:move-cursor (get-window ui)
+                            (get-mem-value-column ui)
+                            display-row)
+        (charms:write-string-at-cursor (get-window ui)
+                                       (third (aref mem-contents i)))))))
+
+  (charms:move-cursor (get-window ui) (get-input-col ui) (get-input-row ui))
+  (charms:refresh-window))
+  
+
+(defmethod hp67-ui:ui-get-input ((ui curses-cli))
+  (let ((output (make-array 0 :element-type 'character :adjustable t))
+        (pos 0))
+    (do ((c (charms:get-char (get-window ui))
+            (charms:get-char (get-window ui))))
+        ((char= c #\Newline))
+      (cond
+        ((rubout-character c)
+         (when (> pos 0)
+           (adjust-array output (1- pos))
+           (charms:move-cursor (get-window ui)
+                               (1- pos)
+                               (get-input-row ui))
+           (charms:write-string-at-cursor (get-window ui) " ")
+           (decf pos)
+           (charms:move-cursor (get-window ui) pos (get-input-row ui))))
+        ((quit-character c)
+         (setf (get-quit-requested ui) t))
+        ((allowed-character c)
+         (adjust-array output (1+ pos))
+         (setf (aref output pos) c)
+         (incf pos)))
+
+      (when (and (= pos 1)
+                 (member c *hot-keys* :test 'char=))
+        (return)))
+
+    output))
+        
+  
+
 
 
 #+sbcl
